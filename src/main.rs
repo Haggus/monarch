f31f0ff5a16d73038b9e48516dc59c8b50fb7fec
Mateusz Mrowiec <mateusz.mrowiec@kotrak.com>
2015-09-11 12:48:55 +0200

    Switched from Vec to HashSet

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@ extern crate image;
 extern crate time;
 
 use std::path::Path;
+use std::collections::HashSet;
 use image::{GenericImage, ImageBuffer, Rgba};
 use std::ops::Sub;
 use time::*;
 
 fn main() {
     let img = image::open(&Path::new("test.png")).unwrap();
-    let mut palette: Vec<Rgba<u8>> = Vec::new();
+    let mut palette:HashSet<Rgba<u8>> = HashSet::new();
 
     let (width, height) = img.dimensions();
 
@@ -18,10 +19,7 @@ fn main() {
     let start = time::now();
     for pixel in img.pixels() {
         let (_,_,pix) = pixel;
-        if is_unique(&mut palette, &pix) {
-            // println!("pixel: {:?}", pixel);
-            palette.push(pix.clone());
-        }
+        palette.insert(pix);
         let percentage = (current_pixel * 100) / total_pixels;
         print!("\r{}% | {} / {}", percentage, current_pixel, total_pixels);
         current_pixel += 1;
@@ -35,44 +33,33 @@ fn main() {
     println!("color: {:?}", img.color());
     println!("width: {:?}, height: {:?}", width, height);
     println!("number of total pixels:  {}", width * height);
-    //println!("{:?} - {:?}", start, end);
     println!("total time: {:?}", total_time);
 }
 
-fn is_unique(palette: &mut Vec<Rgba<u8>>, pixel: &Rgba<u8>) -> bool {
-    let mut does_exist: bool = false;
-    for pix in palette.iter() {
-        if pix == pixel {
-            does_exist = true;
-        }
-    }
-    !does_exist
-}
-
-fn gen_palette(palette: &mut Vec<Rgba<u8>>) -> () {
-    let width:u32;
-    let height: u32;
+fn gen_palette(palette: &mut HashSet<Rgba<u8>>) -> () {
+    let size:u32;
 
     match palette.len() {
         0...2 => {
-            width = 2;
-            height = 2;
+            size = 2;
         },
         _ => {
             let length = palette.len() as f32;
-            width = length.sqrt().ceil() as u32;
-            height = length.sqrt().ceil() as u32;
+            size = length.sqrt().ceil() as u32;
         }
     }
 
-    let mut new_img = ImageBuffer::new(width, height);
+    let mut new_img = ImageBuffer::new(size, size);
 
-    for y in 0..height {
-        for x in 0..width {
-            match palette.pop() {
-                Some(s) => new_img.put_pixel(x, y, s),
-                _ => continue
-            }
+    let mut current_x:u32 = 0;
+    let mut current_y:u32 = 0;
+
+    for pix in palette.iter() {
+        new_img.put_pixel(current_x, current_y, pix.clone());
+        current_x += 1;
+        if current_x == size {
+            current_x = 0;
+            current_y += 1;
         }
     }
 
